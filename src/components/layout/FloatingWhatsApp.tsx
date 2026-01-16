d7181c00e88adeaa27c9875f662f61a3dbaca962
clementinostudio <felipeclementino.dev@gmail.com>
2026-01-16 06:26:22 +0000

    att

--- src/components/layout/FloatingWhatsApp.tsx
+++ src/components/layout/FloatingWhatsApp.tsx
@@ -3,7 +3,7 @@ import { WhatsAppIcon } from '../ui/WhatsAppIcon';
 
 export const FloatingWhatsApp: React.FC = () => {
   return (
-    <a href="#" className="fixed bottom-6 right-6 z-50 group">
+    <a href="https://wa.me/43996536535?text=quero%20minha%20aula%20teste" target="_blank" className="fixed bottom-6 right-6 z-50 group">
       <div className="w-14 h-14 bg-[#22c55e] rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform">
         <WhatsAppIcon className="w-7 h-7 text-white fill-current" />
       </div>
